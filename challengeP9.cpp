add64a1658c1a68e40957178237dabba6367faab
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-27 17:51:08 +0700

    first commit

---
+++ challengeP9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class BelahKetupat;
+class LayangLayang {
+private:
+    //lengkapi Disini
+
+public:
+    void inputData() {
+        //lengkapi Disini
+    }
+
+    double hitungLuas() {
+        return 0.5 * d1 * d2;
+    }
+
+    //lengkapi Disini
+};
+
+class BelahKetupat {
+private:
+    //lengkapi Disini
+}
