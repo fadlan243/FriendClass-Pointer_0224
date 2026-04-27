dcd6ce3e5b2584c89f31f3bbc47c830325e9ffb3
fadlan243 <fadlan.nasrizal17@sma.belajar.id>
2026-04-27 18:04:03 +0700

    method untuk menampilkan class mahasiswa

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -6,6 +6,11 @@ class mahasiswa
 private:   
     string nama;
 public:
-
+    friend void setNama(mahasiswa &a, string);
 };
 
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
